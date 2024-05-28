7665a7fa31c1a56676e4a6d93f51d7a9f57c66da
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-05-28 20:30:22 +0200

    #4 - use RabbitmqSender in controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,15 @@ class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.all
   end
+
+  def create
+    @transaction = Transaction.new
+    if @transaction.save
+      RabbitmqSender.send_transaction(@transaction.id)
+    end
+  end
+
+  def private
+    params.require(:transaction).permit(:amount, :map_id)
+  end
 end
