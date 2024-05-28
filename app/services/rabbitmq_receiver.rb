828219bd808a051a712b8d9eca2904aa89e9666a
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-05-28 20:24:00 +0200

    #4 - error handling

--- app/services/rabbitmq_receiver.rb
+++ app/services/rabbitmq_receiver.rb
@@ -1,18 +1,30 @@
-require 'bunny'
-
 class RabbitmqReceiver
   def self.start
-    connection = Bunny.new
-    connection.start
+    Thread.new do
+      begin
+        connection = Bunny.new(hostname: 'localhost')
+        connection.start
 
-    channel = connection.create_channel
-    queue = channel.queue('transactions')
+        puts "Connected to RabbitMQ"
 
-    queue.subscribe(block: true) do |_delivery_info_, _properties, body|
-      transaction_id = body.to_i
-      TransactionWorker.perform_async(transaction_id)
+        channel = connection.create_channel
+        queue = channel.queue('transactions')
+
+        puts "Subscribed to queue: transactions"
+        puts "Waiting for messages..."
+
+        queue.subscribe(block: true) do |_delivery_info, _properties, body|
+          transaction_id = body.to_i
+          puts "Received transaction ID: #{transaction_id}"
+          TransactionWorker.perform_async(transaction_id)
+        end
+      rescue Interrupt => _
+        connection.close
+        puts "RabbitMQ connection closed."
+      rescue => e
+        puts "An error occurred: #{e.message}"
+        puts e.backtrace.join("\n")
+      end
     end
-
-    puts "Finish"
   end
 end
