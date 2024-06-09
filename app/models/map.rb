a560f5c804b19edf47a1ab3c2d2c2f88f7e715bc
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-06-09 19:44:19 +0200

    #1 - scope map_names

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,5 @@ class Map < ApplicationRecord
   has_many :transactions
 
   # scope :by_name, -> (query) { where("name LIKE ?", "%#{query}%") }
+  scope :map_names, -> { self.map { |map| ["From: #{map.from_exchange_item} To: #{map.where_exchange_item}"]} }
 end
