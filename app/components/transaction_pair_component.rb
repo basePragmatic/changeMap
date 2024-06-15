a21fa04e3ea7dba149db02b20d1055678693a926
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-06-15 13:37:32 +0200

    create map form

--- app/components/transaction_pair_component.rb
+++ app/components/transaction_pair_component.rb
@@ -4,8 +4,4 @@ class TransactionPairComponent < ViewComponent::Base
   def initialize(map)
     @map = map
   end
-
-  def pair
-    "From: #{@map.from_exchange_item} | Where: #{@map.where_exchange_item}"
-  end
 end
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,4 +2,22 @@ class MapsController < ApplicationController
   def index
     @maps = Map.all
   end
+
+  def new
+    @map = Map.new
+  end
+
+  def create
+    @map = Map.new(map_params)
+
+    if @map.save
+      redirect_to maps_path
+    end
+  end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:from_exchange_item, :where_exchange_item)
+  end
 end
