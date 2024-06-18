e5a191a0d06bd2e1ed3ae053684b9249c6ef4e55
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-06-18 20:29:27 +0200

    #16 - user page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :maps
+  resources :users
   resources :transactions
 
   get "accounts", to: "static_pages#accounts", as: :accounts
