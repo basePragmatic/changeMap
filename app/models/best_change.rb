0034ce5b1ff56439775c4960c97d3b71660eddec
Yegor Chsherbakov <yegor.rb@gmail.com>
2024-07-09 20:34:11 +0700

    #2 - db setup

--- app/models/best_change.rb
+++ app/models/best_change.rb
@@ -1,13 +1,5 @@
-require "sequel"
-
+require_relative "db/setup"
 DB = Sequel.sqlite("db/development.sqlite3")
-
-DB.create_table? :bestchanges do
-  primary_key :id
-  String :from_name, unique: true, null: false
-  String :to_name, unique: true, null: false
-  Float :price, null: false
-end
 
 class BestChange < Sequel::Model(DB[:bestchanges])
 end
